18322463eac0ada7d1e472cb50c56225893dc19d
Diego Garcia <dgarciavallejos@gmail.com>
2023-08-08 14:59:38 -0400

    add new components & icons

--- src/components/Svg/RightArrow.tsx
+++ src/components/Svg/RightArrow.tsx
@@ -8,7 +8,7 @@ const RightArrowIcon: React.FC<SvgProps> = ({
   color = '#ffffff',
 }) => {
   const imgSvg = `
-  <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+  <svg width="${width}" height="${height}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1.5" fill="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" fill="${color}"/>
   </svg>
   `;
